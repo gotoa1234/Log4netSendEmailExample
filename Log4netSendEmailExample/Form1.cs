ee8f784c5814d8e7526ccfd773ab44878e765c19
gotoa1234 <cap8825@gmail.com>
2020-09-16 00:48:46 +0800

    Send log4net E-Mail測試範例

---
+++ Log4netSendEmailExample/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Log4netSendEmailExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Log4net發送Mail測試工具");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.Info("今天天氣不錯");
+        }
+    }
+}
